621d8511e666278d752e53c66e9f39e344232d3a
Nate Jones <nate@endot.org>
2014-06-07 18:15:50 -0400

    adding permissions and improved output

--- run.sh
+++ run.sh
@@ -6,21 +6,22 @@
 
 DONEFILE=/pmb_setup_complete
 if [[ ! -e $DONEFILE ]]; then
-    /usr/sbin/rabbitmq-server &> /dev/null &
+    /usr/sbin/rabbitmq-server &
 
     # wait for rabbit to start
     HOSTNAME=$(hostname)
-    /usr/sbin/rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid &> /dev/null
+    /usr/sbin/rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
 
     ADMINPASS=$(pwgen 15 1)
     PASSWORD=$(pwgen 15 1)
     USERNAME=pmb
 
     # create pmb user
-    /usr/sbin/rabbitmqctl add_user $USERNAME $PASSWORD &> /dev/null
+    /usr/sbin/rabbitmqctl add_user $USERNAME $PASSWORD
+    /usr/sbin/rabbitmqctl set_permissions -p / $USERNAME "^$USERNAME-.*" "^$USERNAME-.*" "^$USERNAME-.*"
 
     # set guest password
-    /usr/sbin/rabbitmqctl change_password guest $ADMINPASS &> /dev/null
+    /usr/sbin/rabbitmqctl change_password guest $ADMINPASS
 
     # stop
     /usr/sbin/rabbitmqctl stop &> /dev/null
@@ -35,8 +36,13 @@ Quick Personal Message Bus Docker container.
 
     USER: $USERNAME
     PASS: $PASSWORD
+    ADMIN USER: guest
+    ADMIN PASS: $ADMINPASS
 
-    URL: amqps://${USERNAME}:${PASSWORD}@[hostname]:[port]/
+    CONFIG for PMB:
+
+    export PMB_PRIMARY_URI=amqps://${USERNAME}:${PASSWORD}@[hostname]:[port]/
+    export PMB_SSL_INSECURE_SKIP_VERIFY=1
 
 EOM
 
